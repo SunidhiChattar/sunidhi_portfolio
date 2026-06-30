2dbb3b99296c540482761a1147cf5d301b9fd2f3
sunidhi.c <sunidhi.chattar@iserveu.co.in>
2026-06-30 17:09:16 +0530

    role hide

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -14,16 +14,16 @@ export class ProjectsComponent {
 
     {
       id: 'kotak',
-      title: 'Kotak CBC Portal',
+      title: 'Portal 1',
       description:
-        'Developed and maintained Kotak CBC Portal with responsive UI, custom SVGs, breadcrumb navigation, and interactive D3.js data visualizations, ensuring seamless and secure user experiences.',
+        'Developed and maintained Portal 1 with responsive UI, custom SVGs, breadcrumb navigation, and interactive D3.js data visualizations, ensuring seamless and secure user experiences.',
       tech: ['Angular', 'RxJS', 'REST APIs']
     },
     {
       id: 'nsdl',
-      title: 'NSDL CBC Platform',
+      title: 'Portal 2',
       description:
-        'Built and optimized NSDL CBC Platform’s web interface, enabling seamless multi-level navigation, enhanced user engagement, and secure, high-performance interactions.',
+        'Built and optimized Portal 2’s web interface, enabling seamless multi-level navigation, enhanced user engagement, and secure, high-performance interactions.',
       tech: ['Angular', 'RxJS', 'REST APIs']
     },
     {
@@ -34,14 +34,14 @@ export class ProjectsComponent {
     },
     {
       id: 'bob',
-      title: 'Bank of Baroda Mobile App',
+      title: 'React Native Mobile App',
       description:
-        'Developed the Bank of Baroda cross-platform React Native application, replicating the existing Android product with end-to-end API integration, dynamic dashboard rendering, secure data handling, session management, and management of debug and release APK configurations.',
+        'Developed a cross-platform React Native application with end-to-end API integration, dynamic dashboard rendering, secure data handling, session management, and management of debug and release APK configurations.',
       tech: ['React Native', 'APIs', 'TypeScript']
     },
     {
       id: 'android',
-      title: 'FINO Bank Prepaid Card System',
+      title: 'Android App',
       description:
         'Developed using Kotlin and Jetpack Compose, implementing biometric authentication, encryption, and modular, lifecycle-aware UI components for secure and efficient user experiences.',
       tech: ['Angular', 'RxJS', 'REST APIs']
